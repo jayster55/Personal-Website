e6961314f3a5eec5278671ba7032178ca5b939d0
Jared Gee <jaredgee.main@gmail.com>
2017-04-02 15:36:19 -0400

    Added Technical Expertise Tab

--- src/Home.js
+++ src/Home.js
@@ -1,62 +1,23 @@
 import React, { Component } from 'react';
-import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
-import {GridList, GridTile} from 'material-ui/GridList';
-import FlatButton from 'material-ui/FlatButton';
+import {Card, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
 import { Grid, Row, Col } from 'react-flexbox-grid';
-import Divider from 'material-ui/Divider';
-import {List, ListItem} from 'material-ui/List';
-import IconButton from 'material-ui/IconButton';
-import FontIcon from 'material-ui/FontIcon';
 import Barbell from 'material-ui/svg-icons/places/fitness-center';
-
-import Guy from 'material-ui/svg-icons/action/face';
 import GradHat from 'material-ui/svg-icons/social/school';
-import Barbell from 'material-ui/svg-icons/places/fitness-center';
-
+import Star from 'material-ui/svg-icons/action/stars';
 import profile from './img/profilephoto.jpg';
 import UW from './img/uw.png';
 import WLU from './img/wlu.png';
-
-const styles = {
-  smallIcon: {
-    width: 36,
-    height: 36,
-  },
-  mediumIcon: {
-    width: 48,
-    height: 48,
-  },
-  largeIcon: {
-    width: 60,
-    height: 60,
-  },
-  small: {
-    width: 72,
-    height: 72,
-    padding: 16,
-  },
-  medium: {
-    width: 96,
-    height: 96,
-    padding: 24,
-  },
-  large: {
-    width: 120,
-    height: 120,
-    padding: 30,
-  },
-};
 
 export default class Home extends Component {
 
   render() {
     return (
-      <Grid style={{marginTop: "10px"}}>
+      <Grid style={{marginTop: "10px", marginBottom: "-1000px"}}>
         <Row middle="xs">
           <Col md={5}>
            <Card>
             <CardMedia>
-              <img src={profile}/>
+              <img src={profile} alt="Profile"/>
             </CardMedia>
             <CardTitle 
               title="I am Jared Gee" 
@@ -69,7 +30,6 @@ export default class Home extends Component {
                 title={
                   <div>
                     About Me
-                    <Guy />
                   </div>
                 }
               />
@@ -93,11 +53,11 @@ export default class Home extends Component {
               />
               <CardText>
                 <Row middle="xs" style={{marginLeft: "50px"}}>
-                  <Col center="xs" xs>
-                    <img src={UW}/>
+                  <Col xs>
+                    <img src={UW} alt="UW logo"/>
                   </Col>
-                  <Col center="xs" xs>
-                    <img src={WLU}/>
+                  <Col xs>
+                    <img src={WLU} alt="WLU logo"/>
                   </Col>
                 </Row>
                 <div style={{marginLeft: "15px"}}>
@@ -113,7 +73,7 @@ export default class Home extends Component {
                 title={
                   <div>
                     Scholarships & Awards
-                    <ActionFlightTakeoff />
+                    <Star style={{height: "20px", marginTop: "10px"}} />
                   </div>
                 }
               />
--- src/Navigation.js
+++ src/Navigation.js
@@ -1,22 +1,61 @@
 import React, { Component } from 'react';
 import {Tabs, Tab} from 'material-ui/Tabs';
 import injectTapEventPlugin from 'react-tap-event-plugin';
+import SwipeableViews from 'react-swipeable-views';
+
+
 import Home from './Home';
+import TechnicalExpertise from './TechnicalExpertise';
+import Projects from './Projects';
+
 injectTapEventPlugin();
 
 
 export default class Navigation extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      slideIndex: 0,
+    };
+  }
+
+  handleChange = (value) => {
+    this.setState({
+      slideIndex: value,
+    });
+  };
+
   render() {
     return (
-      <Tabs>
-        <Tab label="About Me">
-          <Home />
-        </Tab>
-        <Tab label="Technical Expertise" />
-        <Tab label="Projects" />
-        <Tab label="Impossible List" />
-      </Tabs>
+      <div>
+        <Tabs
+          onChange={this.handleChange}
+          value={this.state.slideIndex}
+        >
+          <Tab label="About Me" value={0} />
+          <Tab label="Technical Expertise" value={1} />
+          <Tab label="Projects" value={2} />
+          <Tab label="Impossible List" value={2} />
+        </Tabs>
+        <SwipeableViews
+          index={this.state.slideIndex}
+          onChangeIndex={this.handleChange}
+        >
+          <div>
+            <Home />
+          </div>
+          <div >
+            <TechnicalExpertise />
+          </div>
+          <div>
+            <Projects />
+          </div>
+          <div>
+            slide n°3
+          </div>
+        </SwipeableViews>
+      </div>
     );
   }
 }
